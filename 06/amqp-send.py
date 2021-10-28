84f63da8366f388427bdcc30657b1ddc65c8fa1b
gil612 <sgba000@gmail.com>
2021-10-28 12:12:16 +0200

    102

--- 06/amqp-send.py
+++ 06/amqp-send.py
@@ -5,14 +5,14 @@
 import pika
 from datetime import datetime
 # connect to RabbitMQ-Broker on localhost
-connection=pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel=connection.channel()
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
 
 channel.queue_declare(queue='tq')
 
 
-# publish the message (body) to the queue (routing_key)
-t0 = str(datetime.now()).encode('utf-8')
+t = str(datetime.now()).encode('utf-8')
 for x in range(0,10000):
     channel.basic_publish(exchange ='',
                         routing_key= 'tq',
@@ -20,7 +20,7 @@ for x in range(0,10000):
     
 channel.basic_publish(exchange = '',
                         routing_key =  'tq',
-                        body = t0)
+                        body = t)
 
 
 # close the connection
